80e13f3241d7a7bb7b674671a5b27f96da628244
Yasar Senturk <yasarix@gmail.com>
2011-04-03 05:28:59 -0400

    Added script description and license

--- total_diff.php
+++ total_diff.php
@@ -2,6 +2,22 @@
 
 <?php
 
+/**
+ * This script finds differences between two files and writes into the output
+ * file. Note that this script only finds the lines that does not exist in
+ * second file.
+ * 
+ * Copyright (C) 2011 Yasar Senturk <yasarix@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * @author Yasar Senturk <yasarix@gmail.com>
+ * @version 1.0
+ */
+
 if ($argc != 4) {
 	echo "Parameter Error!\n";
 	echo "Usage:\n";
